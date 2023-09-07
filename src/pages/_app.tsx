1deef76eebe33cf686e25265208f46ca8d769f58
h.higuchi.93@gmail.com <h.higuchi.93@gmail.com>
2023-09-07 23:10:21 +0900

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import {
+  WalletProvider,
+} from '@suiet/wallet-kit';
+import '@suiet/wallet-kit/style.css';
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WalletProvider>
+      <Component {...pageProps} />
+    </WalletProvider>
+  )
 }
