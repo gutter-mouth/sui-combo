4937770981f261208bfe174f76728a6eec7e97b5
guttermouth <8745498+gutter-mouth@users.noreply.github.com>
2023-09-09 14:36:58 +0900

    Revert "LendingのmoveCall完成 (#6)"
    This reverts commit 3dee08d4c22e84e3910489ea7e54e4c3ec1dd4d9.

--- src/utils/moveCall/moveCall.ts
+++ src/utils/moveCall/moveCall.ts
@@ -1,18 +1,16 @@
 
-import { deposit, borrow, withdraw } from "./naviProtocol";
+import { deposit, borrow } from "./naviProtocol";
 import type { NaviProps } from "./naviProtocol";
 
 
 export type MoveCallProps =
   NaviProps & {
-    method: "deposit" | "borrow" | "withdraw";
+    method: "deposit" | "borrow";
   };
 
-export const moveCall = ({ method, tx, coinType, amount, balances, recipient }: MoveCallProps) => {
+export const moveCall = ({ method, tx, token, amount }: MoveCallProps) => {
   if (method === "deposit")
-    return deposit({ tx, coinType, amount, balances });
+    return deposit({ tx, token, amount });
   if (method === "borrow")
-    return borrow({ tx, coinType, amount });
-  if (method === "withdraw")
-    return withdraw({ tx, coinType, amount, recipient });
+    return borrow({ tx, token, amount });
 }
